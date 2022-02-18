a36cc0339a4079296a5f4186c85f755b17f044f9
沈文骐 <shenwenqi@uniontech.com>
2022-02-18 14:29:53 +0800

    feat: 单元测试结果目录结构调整
    修改单元测试结果目录结构
    
    Log: 单元测试结果目录结构调整
    Task: https://pms.uniontech.com/zentao/task-view-100126.html
    Influence: 单元测试输出结果
    Change-Id: Ic63cb9c3651f43b30c562b5d0a731ebfd08cb1aa

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BUILD_DIR=build
+BUILD_DIR=build-ut
+HTML_DIR=html
 REPORT_DIR=report
 
 cd ../
@@ -11,16 +12,19 @@ cd $BUILD_DIR
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -j 8
 
+mkdir -p ../tests/$HTML_DIR
+mkdir -p ../tests/$REPORT_DIR
+
 lcov -c -i -d ./ -o init.info
-./ut-dde-clipboard --gtest_output=xml:ut-dde-clipboard.xml
-./ut-dde-clipboardloader --gtest_output=xml:ut-dde-clipboardloader.xml
+./ut-dde-clipboard --gtest_output=xml:./$REPORT_DIR/ut-report_dde-clipboard.xml
+./ut-dde-clipboardloader --gtest_output=xml:./$REPORT_DIR/ut-report_dde-clipboardloader.xml
 lcov -c -d ./ -o cover.info
 lcov -a init.info -a cover.info -o total.info
 lcov -r total.info "*/tests/*" "*/usr/include*" '*tests*' -o final.info
 
-rm -rf ../tests/$REPORT_DIR
-mkdir -p ../tests/$REPORT_DIR
-genhtml -o ../tests/$REPORT_DIR final.info
+genhtml -o ./$HTML_DIR final.info
+
+mv ./$HTML_DIR/index.html ./$HTML_DIR/cov_dde-clipboard.html
 
 mv asan_board.log* asan_dde-clipboard.log
 mv asan_loader.log* asan_dde-clipboardloader.log
