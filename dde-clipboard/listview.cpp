f5ac1297d3705e473dbbb5a7ed78f1f8dfb5c9b7
shaojun <wangshaojun@uniontech.com>
2022-09-06 15:03:38 +0800

    chore: adapt license and copyright and readme (#114)
    Modify project to pass the REUSE check.
    Add readme files.
    
    Log: Modify project to pass the REUSE check,and add readme files.
    Task: https://pms.uniontech.com/task-view-185215.html

--- dde-clipboard/listview.cpp
+++ dde-clipboard/listview.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "listview.h"
 #include "refreshtimer.h"
 #include "constants.h"
