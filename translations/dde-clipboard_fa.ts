61fa0657ccf77ba6567a738a77daf1876b5d0bc0
Robertkill <lvpeilong@uniontech.com>
2025-11-05 11:25:06 +0800

    fix: Update translate
    Update translate
    
    Log: Update translate
    pms: BUG-339669

--- translations/dde-clipboard_fa.ts
+++ translations/dde-clipboard_fa.ts
@@ -1,4 +1,27 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="fa">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fa">
+<context>
+    <name>ClipboardItem</name>
+    <message>
+        <location filename="../dock-clipboard-plugin/clipboarditem.cpp" line="33"/>
+        <source>Clipboard</source>
+        <translation type="unfinished">کلیپ بورد</translation>
+    </message>
+    <message>
+        <location filename="../dock-clipboard-plugin/clipboarditem.cpp" line="64"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ClipboardPlugin</name>
+    <message>
+        <location filename="../dock-clipboard-plugin/clipboardplugin.cpp" line="44"/>
+        <source>Clipboard</source>
+        <translation type="unfinished">کلیپ بورد</translation>
+    </message>
+</context>
 <context>
     <name>ItemData</name>
     <message>
@@ -19,49 +42,49 @@
     <message>
         <location filename="../dde-clipboard/itemdata.cpp" line="146"/>
         <source>%1 characters</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>ItemWidget</name>
     <message>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="367"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="369"/>
         <source>%1 files (%2...)</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="418"/>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="607"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="420"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="609"/>
         <source>(File deleted)</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="441"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="443"/>
         <source>Yesterday</source>
         <translation>دیروز</translation>
     </message>
     <message>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="447"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="449"/>
         <source>Just now</source>
         <translation>همین حالا</translation>
     </message>
     <message>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="449"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="451"/>
         <source>1 minute ago</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="451"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="453"/>
         <source>%1 minutes ago</source>
         <translation>%1 دقیقه پیش</translation>
     </message>
     <message>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="453"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="455"/>
         <source>1 hour ago</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-clipboard/itemwidget.cpp" line="455"/>
+        <location filename="../dde-clipboard/itemwidget.cpp" line="457"/>
         <source>%1 hours ago</source>
         <translation>%1 ساعت پیش</translation>
     </message>
@@ -79,9 +102,9 @@
         <translation>همه را پاک کن</translation>
     </message>
     <message>
-        <location filename="../dde-clipboard/mainwindow.cpp" line="328"/>
+        <location filename="../dde-clipboard/mainwindow.cpp" line="329"/>
         <source>Copy the content to clipboard</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
